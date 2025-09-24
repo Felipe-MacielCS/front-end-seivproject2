e1cfbd12a114a80bec6a66ddae38d18ea53ba9d8
Felipe-MacielCS <122392496+Felipe-MacielCS@users.noreply.github.com>
2025-09-24 09:37:46 -0500

    Header Front End added

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app-bar color="primary" density="comfortable" flat>
+    <v-container fluid class="d-flex align-center ga-3">
+      <v-img src="/src/assets/avocado.png" alt="custom" max-width="28" max-height="28" />
+      <span class="text-h6 ml-2">Course list</span>
+
+        <v-combobox
+        label="Major"
+        :items="['CS', 'MED', 'ADMIN']"
+        density="compact"
+        variant="tonal"
+        hide-details
+        style="max-width: 120px"
+        ></v-combobox>
+
+        <v-combobox
+        label="Level"
+        :items="['CS', 'MED', 'ADMIN']"
+        density="compact"
+        variant="tonal"
+        hide-details
+        style="max-width: 120px"
+        ></v-combobox>
+
+        <v-combobox
+        label="Credits"
+        :items="['CS', 'MED', 'ADMIN']"
+        density="compact"
+        variant="tonal"
+        hide-details
+        style="max-width: 120px"
+        ></v-combobox>
+
+        <v-btn icon variant="tonal" rounded="lg" class="ml-2">
+        <v-icon>mdi-plus</v-icon>
+        </v-btn>
+
+        <v-btn icon variant="tonal" rounded="lg" class="ml-2">
+        <v-icon>mdi-download</v-icon>
+        </v-btn>
+
+        <v-spacer />
+
+        <v-text-field
+        label="Search courses"
+        prepend-inner-icon="mdi-magnify"
+        variant="tonal"
+        density="comfortable"
+        hide-details
+        clearable
+        rounded="lg"
+        style="max-width: 360px"
+        />
+
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script setup>
+
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,6 +14,15 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#7357f0',
+          surface: '#cbbefcff',   
+        },
+      },
+    },
   },
-})
+  icons: { defaultSet: 'mdi' },
+});
