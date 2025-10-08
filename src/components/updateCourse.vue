f7f5ae845ad10c28b7188ecf4b11d830e8675d56
carlen.gattenby <carlen.gattenby@eagles.oc.edu>
2025-10-08 17:51:47 -0500

    fixed cancel button

--- src/components/updateCourse.vue
+++ src/components/updateCourse.vue
@@ -13,21 +13,30 @@ const dialogVisible = computed({
   set: (value) => emit('update:modelValue', value)
 })
 
-const editedCourse = ref({
-  course_name: '',
-  course_number: '',
-  dept: '',
-  level: '',
-  hours: '',
-  course_description: ''
-})
+function emptyCourse() {
+  return {
+    course_name: '',
+    course_number: '',
+    dept: '',
+    level: '',
+    hours: '',
+    course_description: ''
+  }
+}
+
+const editedCourse = ref(emptyCourse())
+
+const form = ref(null)
 
 
 watch(
-  () => props.course,
-  (newCourse) => {
-    if (newCourse) {
-      editedCourse.value = { ...newCourse }
+   () => props.modelValue,
+  (open) => {
+    if (open) {
+      editedCourse.value = props.course ? JSON.parse(JSON.stringify(props.course)) : emptyCourse()
+      if (form.value && typeof form.value.resetValidation === 'function') {
+        form.value.resetValidation()
+      }
     }
   },
   { immediate: true }
@@ -36,16 +45,20 @@ watch(
 const rules = {
   required: (v) => !!v || 'This field is required',
   number: (v) => !isNaN(Number(v)) || 'Must be a number',
-  positive: (v) => Number(v) > 0 || 'Must be greater than 0',
+  nonNegative: (v) => Number(v) >= 0 || 'Must be 0 or greater',
   length: (min) => (v) =>
     (v && v.length >= min) || `Must be at least ${min} characters`
 }
 
 
 function saveChanges() {
-  
- if (!isValid.value) return
+  if (!isValid.value) return
   emit('save', editedCourse.value)
+  dialogVisible.value = false
+}
+
+function cancelChanges() {
+  editedCourse.value = props.course ? JSON.parse(JSON.stringify(props.course)) : emptyCourse()
   dialogVisible.value = false
 }
 
@@ -78,7 +91,7 @@ const isValid = computed(() => {
           <v-text-field
             v-model="editedCourse.course_number"
             label="Course Number"
-            :rules="[rules.required, rules.number]"
+            :rules="[rules.required]"
             required
           ></v-text-field>
 
@@ -92,30 +105,28 @@ const isValid = computed(() => {
           <v-text-field
             v-model="editedCourse.level"
             label="Level"
-            :rules="[rules.required, rules.number, rules.positive]"
+            :rules="[rules.required, rules.number]"
             required
           ></v-text-field>
 
           <v-text-field
             v-model="editedCourse.hours"
             label="Credit Hours"
-            :rules="[rules.required, rules.number, rules.positive]"
+            :rules="[rules.required, rules.number]"
             required
           ></v-text-field>
 
           <v-textarea
             v-model="editedCourse.course_description"
             label="Description"
-            :rules="[rules.required]"
-            required
           ></v-textarea>
         </v-form>
       </v-card-text>
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn text @click="dialogVisible = false">Cancel</v-btn>
-        <v-btn color="primary" :disabled="!isValid" @click="saveCourse">Save</v-btn>
+        <v-btn text @click="cancelChanges">Cancel</v-btn>
+        <v-btn color="primary" :disabled="!isValid" @click="saveChanges">Save</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
